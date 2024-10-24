3aaf95bf04f84e0de2ea53e78fe11b18e45bc42a
Vinicius dos Santos <153734940+Vini0019@users.noreply.github.com>
2024-10-23 22:55:02 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,258 @@
+window.revelar = ScrollReveal({ reset: true })
+
+// Topo do site
+revelar.reveal('.efeito-txt-topo',
+    {
+        duration: 2000,
+        distance: '90px',
+        // origin: left,
+        reset: false,
+    })
+
+revelar.reveal('.efeito-txt-button',
+    {
+        duration: 2000,
+        distance: '90px',
+        reset: false
+
+    })
+
+revelar.reveal('.efeito-txt-img',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left',
+        reset: false
+
+    })
+
+revelar.reveal('.efeito-txt-historia',
+    {
+        duration: 2000,
+        distance: '100%',
+        origin: 'left',
+        reset: false,
+        opacity: 1
+
+    })
+
+revelar.reveal('.efeito-img-lampada',
+    {
+        duration: 3000,
+        distance: '20%',
+        origin: 'bottom',
+        reset: false,
+        opacity: 0
+
+    })
+
+revelar.reveal('.efeito-img-eletricista1',
+    {
+        duration: 2000,
+        distance: '100%',
+        origin: 'top',
+        reset: false,
+        opacity: 0
+
+    })
+
+revelar.reveal('.efeito-img-eletricista2',
+    {
+        duration: 2000,
+        distance: '40%',
+        origin: 'right',
+        reset: false,
+        opacity: 0
+
+    })
+
+revelar.reveal('.efeito-img-eletricista3',
+    {
+        duration: 2000,
+        distance: '100%',
+        origin: 'bottom',
+        reset: false,
+        opacity: 0
+
+    })
+
+revelar.reveal('.efeito-img-instagram',
+    {
+        duration: 2000,
+        distance: '100%',
+        origin: 'bottom',
+        reset: false,
+        opacity: 0
+
+    })
+
+revelar.reveal('.efeito-txt-servicos',
+    {
+        duration: 2000,
+        distance: '5%',
+        origin: 'left',
+        reset: false,
+        opacity: 0
+
+    })
+
+revelar.reveal('.efeito-txt-orcamento',
+    {
+        duration: 1000,
+        distance: '100%',
+        origin: 'bottom',
+        reset: false,
+        opacity: 0
+
+    })
+
+revelar.reveal('.efeito-txt-contador',
+    {
+        duration: 1000,
+        distance: '100%',
+        delay: 200,
+        origin: 'bottom',
+        reset: false,
+        opacity: 0
+
+    })
+revelar.reveal('.efeito-txt-fiacao',
+    {
+        duration: 1000,
+        distance: '100%',
+        delay: 400,
+        origin: 'bottom',
+        reset: false,
+        opacity: 0
+
+    })
+
+revelar.reveal('.efeito-txt-residencial',
+    {
+        duration: 1000,
+        distance: '100%',
+        delay: 600,
+        origin: 'bottom',
+        reset: false,
+        opacity: 0
+
+    })
+revelar.reveal('.efeito-txt-painel',
+    {
+        duration: 1000,
+        distance: '100%',
+        delay: 800,
+        origin: 'bottom',
+        reset: false,
+        opacity: 0
+
+    })
+revelar.reveal('.efeito-txt-motores',
+    {
+        duration: 1000,
+        distance: '100%',
+        delay: 1000,
+        origin: 'bottom',
+        reset: false,
+        opacity: 0
+
+    })
+revelar.reveal('.efeito-txt-agende',
+    {
+        duration: 1000,
+        distance: '5%',
+        origin: 'left',
+        reset: false,
+        opacity: 1
+
+    })
+revelar.reveal('.efeito-txt-clientes',
+    {
+        duration: 1000,
+        distance: '5%',
+        origin: 'left',
+        reset: false,
+        opacity: 1
+
+    })
+revelar.reveal('.efeito-txt-opniao1',
+    {
+        duration: 1000,
+        distance: '50%',
+        origin: 'bottom',
+        reset: false,
+        opacity: 1
+
+    })
+revelar.reveal('.efeito-txt-opniao2',
+    {
+        duration: 1000,
+        distance: '50%',
+        delay: 200,
+        origin: 'bottom',
+        reset: false,
+        opacity: 1
+
+    })
+revelar.reveal('.efeito-txt-opniao3',
+    {
+        duration: 1000,
+        distance: '50%',
+        delay: 400,
+        origin: 'bottom',
+        reset: false,
+        opacity: 1
+
+    })
+revelar.reveal('.efeito-txt-agendar',
+    {
+        duration: 1000,
+        distance: '100%',
+        origin: 'top',
+        reset: false,
+        opacity: 0
+
+    })
+revelar.reveal('.efeito-img-eletricista-agendar',
+    {
+        duration: 1000,
+        distance: '100%',
+        origin: 'left',
+        reset: false,
+        opacity: 0
+
+    })
+
+revelar.reveal('.efeito-txt-contato',
+    {
+        duration: 1000,
+        distance: '20%',
+        origin: 'left',
+        reset: false,
+        opacity: 0
+
+    })
+
+
+
+
+
+// const myObserver = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show')
+//         } else {
+//             entry.target.classList.remove('.show')
+//         }
+//     })
+// })
+
+// const elements = document.querySelectorAll('.hidden')
+
+// elements.forEach((element) => myObserver.observe(element))
+
+
+
+
+
